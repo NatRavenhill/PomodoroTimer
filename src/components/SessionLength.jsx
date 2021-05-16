43573f9d02a75a110b63c3717789082a2531c3c0
NatRavenhill <natalie.ravenhill@googlemail.com>
2021-05-16 21:39:38 +0100

    Convert some components to functional components

--- src/components/SessionLength.jsx
+++ src/components/SessionLength.jsx
@@ -1,36 +1,21 @@
-import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle, faMinusCircle } from "@fortawesome/free-solid-svg-icons";
 
-class SessionLength extends Component {
-  handleSessionIncrement = () => {
-    this.props.handleSessionIncrement();
-  };
+function SessionLength(props) {
+  const { handleSessionDecrement, handleSessionIncrement, sessionLength } = props
 
-  handleSessionDecrement = () => {
-    this.props.handleSessionDecrement();
-  };
-
-  render() {
-    return (
-      <div className="Session-length">
-        <h2 id="session-label">Session Length</h2>
-        <h3 id="session-length">{this.props.sessionLength}</h3>
-        <button
-          id="session-decrement"
-          onClick={this.handleSessionDecrement.bind(this)}
-        >
-          <FontAwesomeIcon icon={faMinusCircle} size="2x" />
-        </button>
-        <button
-          id="session-increment"
-          onClick={this.handleSessionIncrement.bind(this)}
-        >
-          <FontAwesomeIcon icon={faPlusCircle} size="2x" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="Session-length">
+      <h2 id="session-label">Session Length</h2>
+      <h3 id="session-length">{sessionLength}</h3>
+      <button id="session-decrement" onClick={handleSessionDecrement}>
+        <FontAwesomeIcon icon={faMinusCircle} size="2x" />
+      </button>
+      <button id="session-increment" onClick={handleSessionIncrement}>
+        <FontAwesomeIcon icon={faPlusCircle} size="2x" />
+      </button>
+    </div>
+  );
 }
 
 export default SessionLength;
--- src/components/Tomato.jsx
+++ src/components/Tomato.jsx
@@ -1,39 +1,25 @@
-import { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faPlay,
-  faPause,
-  faRedo,
-} from "@fortawesome/free-solid-svg-icons";
-import './Tomato.css';
+import { faPlay, faPause, faRedo } from "@fortawesome/free-solid-svg-icons";
+import "./Tomato.css";
 
-class Tomato extends Component {
+function Tomato (props) {
+  const { handleReset, handleStartStop, minutes, seconds } = props;
 
-  handleStartStop = () => {
-    this.props.handleStartStop();
-  };
-
-  handleReset = () => {
-    this.props.handleReset();
-  };
- 
-  render() {
-    return (
-      <div className="tomato">
-        <p id="time-left">
-          {this.props.minutes.toString().padStart(2, "0")}:
-          {this.props.seconds.toString().padStart(2, "0")}
-        </p>
-        <button id="start_stop" onClick={this.handleStartStop.bind(this)}>
-          <FontAwesomeIcon icon={faPlay} color="#FDE9B0" size="2x" />
-          <FontAwesomeIcon icon={faPause} color="#FDE9B0" size="2x" />
-        </button>
-        <button id="reset" onClick={this.handleReset.bind(this)}>
-          <FontAwesomeIcon icon={faRedo} color="#FDE9B0" size="2x" />
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div className="tomato">
+      <p id="time-left">
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}
+      </p>
+      <button id="start_stop" onClick={handleStartStop}>
+        <FontAwesomeIcon icon={faPlay} color="#FDE9B0" size="2x" />
+        <FontAwesomeIcon icon={faPause} color="#FDE9B0" size="2x" />
+      </button>
+      <button id="reset" onClick={handleReset}>
+        <FontAwesomeIcon icon={faRedo} color="#FDE9B0" size="2x" />
+      </button>
+    </div>
+  );
 }
 
 export default Tomato;
